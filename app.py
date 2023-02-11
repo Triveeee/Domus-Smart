564ace0f707e61f2d3511f9d9a36abb4ea547db5
Triveeee <ricardo.trivellato2004@gmail.com>
2023-02-11 09:59:16 +0100

    aggiunta conigurazione flask  mysql

--- app.py
+++ app.py
@@ -4,8 +4,14 @@ from flaskext.mysql import MySQL
 app = Flask(__name__)
 app.config[MYSQL_DATABASE_USER] = '5ai-trivellato'
 app.config[MYSQL_DATABASE_PASSWORD] = '5ai-trivellato'
-app.config[MYSQL_DATABASE_HOST] = '192.168'
+app.config[MYSQL_DATABASE_HOST] = '192.168.5.37'
+app.config[MYSQL_DATABASE_DB] = '5ai-trivellato'
 
 mysql = MySQL()
-
 mysql.init_app(app)
+
+cursor = mysql.get_db().cursor()
+
+@app.route('login')
+def login():
+    return(render_template('login_screen.html'))
